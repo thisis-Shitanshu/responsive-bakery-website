43bb5109ea107a9c6ea6176c13b33254cf76e957
Shitanshu <shitanshu.19p@gmail.com>
2019-08-31 13:36:17 +0530

    Completed.

--- scripts.js
+++ scripts.js
@@ -4,4 +4,9 @@ $(function() {
         $('.toggle').toggleClass('open');
         $('.nav-list').toggleClass('open');
     });
+
+    AOS.init({
+        easing: 'ease',
+        duration: 1000
+    });
 });
